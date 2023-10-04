144679013fed310ce6fbb1dc9c846d7e85d3edf6
SaharMach <saharmach28@gmail.com>
2023-10-04 14:01:04 +0300

    F/home-page/base styling

--- src/cmps/HomeEnterSection.jsx
+++ src/cmps/HomeEnterSection.jsx
@@ -3,14 +3,14 @@ export function HomeEnterSection(){
     return (
         <section className='enter-section'>
             <section className="enter-section-details">
-                <h2>Trello brings all your tasks, teammates, and tools
+                <h1>Trello brings all your tasks, teammates, and tools
                     together
-                </h2>
-                <h3>
+                </h1>
+                <p>
                     Keep everything in the same place-even if your team isn't.
-                </h3>
-                <button>Start demo</button><br/>
-                <a href="#">Watch video <img src="https://res.cloudinary.com/dpwmxprpp/image/upload/v1696379493/asset_50_epbd6c.svg" alt="" /></a>
+                </p>
+                <button>Start demo</button>
+                {/* <a href="#">Watch video <img src="https://res.cloudinary.com/dpwmxprpp/image/upload/v1696379493/asset_50_epbd6c.svg" alt="" /></a> */}
             </section>
             <section className="enter-section-img">
             <img src="https://images.ctfassets.net/rz1oowkt5gyp/75rDABL8fyMtNLlUAtBxrg/c5e145977a86c41c47e17c69410c64f7/TrelloUICollage_4x.png?w=2280&fm=webp" alt="" />
