94dbbee3a9910fbdcfb3580d58e30d5126383853
Maya <mayac8771108@gmail.com>
2023-10-04 18:25:47 +0300

    F/AppHeader/added app header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,8 +1,9 @@
 import { Link, NavLink } from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 // import { login, logout, signup } from '../store/user.actions.js'
 import { LoginSignup } from '../pages/LoginSignup'
+import { appHeaderSvg } from './Svgs'
 
 export function AppHeader() {
 
@@ -32,13 +33,27 @@ export function AppHeader() {
     // }
 
     return (
-        <header className="app-header">
-            <nav className='nav-container'>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/workspace">Workspace</NavLink>
-                <NavLink to="/login">Log in</NavLink>
-            </nav>
-            <h1>My App</h1>
+        <header className='app-header'>
+            <section className='nav-links'>
+                <button className='app-header-btn nav-link-btn menu'>{appHeaderSvg.menu}</button>
+                <img className='logo' src='https://res.cloudinary.com/dehxwadkk/image/upload/v1696425587/logo_vuo3fy.gif' alt="" />
+                <section className='links'>
+                <button className='app-header-btn nav-link-btn link'><span>Workspaces</span> {appHeaderSvg.arrowDown}</button>
+                <button className='app-header-btn nav-link-btn link'><span>Recent</span> {appHeaderSvg.arrowDown}</button>
+                <button className='app-header-btn nav-link-btn link'> <span>Starred</span> {appHeaderSvg.arrowDown}</button>
+                <button className='app-header-btn nav-link-btn link'>Templats {appHeaderSvg.arrowDown}</button>
+                </section>
+                <button className='create-btn'>Create</button>
+            </section>
+
+            <section className='nav-info'>
+                <div className='search-input'>
+                    <span>{appHeaderSvg.search}</span>
+                    <input type="text" placeholder='Search' />
+                </div>
+                <button className='app-header-btn user-info'>{appHeaderSvg.notifications}</button>
+                <img className='app-header-btn user-info' src="https://res.cloudinary.com/dpwmxprpp/image/upload/v1696367658/1642589384427_hywpod.jpg" alt="" />
+            </section>
         </header>
     )
 }
