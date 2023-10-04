c42f74b0ea8ec67417d47dedc9036306dfa3b793
SaharMach <saharmach28@gmail.com>
2023-10-04 04:37:33 +0300

    F/home-page/home page css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { HomeEnterSection } from '../cmps/HomeEnterSection'
 export function HomePage() {
 
 
     return (
-        <div>Home Page</div>
+        <div className='main-container'>
+            <HomeEnterSection />
+            <img className='home-wave full' src="https://images.ctfassets.net/rz1oowkt5gyp/6bFuTiN5YQ78rFkHb3aJlo/b608362b08e14f5dec4c4aeea3a3ca72/white-wave-mobile.svg" alt="" />
+        </div>
     )
 }
