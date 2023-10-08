923271d4d3c0c48181f3410480225a66547709fe
SaharMach <saharmach28@gmail.com>
2023-10-09 01:54:33 +0300

    F/board-sidebar/starting

--- src/cmps/BoardSidebar.jsx
+++ src/cmps/BoardSidebar.jsx
@@ -1,10 +1,20 @@
+import { sideBar } from "./Svgs"
+import { useState } from "react"
 export function BoardSidebar(){
+    const [isSidebarExpand, setSidebarExpand] = useState(false)
+    
+
+
     return (
-        <section className="board-sidebar">
-            <ul className="clean-list">
+        <section className='board-sidebar'>
+            {/* <header>LOGO</header> */}
+            <ul className={`clean-list ${isSidebarExpand ? 'expand' : ''}`}>
+                
                 <li>
-                    Your boards
+                    
                     {/* TODO - RENDER BOARD  */}
+                    <button className="expand-btn clean-btn"
+                     onClick={() => setSidebarExpand(!isSidebarExpand)}>{'>'}</button>
                 </li>
             </ul>
         </section>
