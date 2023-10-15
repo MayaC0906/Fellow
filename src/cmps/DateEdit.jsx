d495338ae01c22676eeb9934cf3f32cfad5bef0d
reut <reutedry1@gmail.com>
2023-10-15 23:14:18 +0300

    F/due-date/Dates comp css + setting checkbox functionality

--- src/cmps/DateEdit.jsx
+++ src/cmps/DateEdit.jsx
@@ -1,24 +1,23 @@
 import { DateCalendar, DateField, DatePicker, TimeField } from '@mui/x-date-pickers'
 import { additionTaskSvg } from './Svgs'
-// import { DemoItem } from '@mui/x-date-pickers/internals/demo'
 import dayjs from 'dayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-// import DateTimeFieldValue from './DueDate';
-import { useEffect, useState } from 'react'
-// import { auto } from '@popperjs/core';
+import { useEffect, useRef, useState } from 'react'
 import { BasicDateField } from './DueDate'
 import { BasicTimeField } from './DueTime'
 import { useParams } from 'react-router'
 import { Button } from '@mui/joy'
 import { loadTask, removeDueDate, saveTaskDueDate } from '../store/actions/board.actions'
-import { boardService } from '../services/board.service.local'
+import { Checkbox } from '@mui/material'
 
 
 
 export function DateEdit({ editName, onCloseEditTask, setTask }) {
     const [selectedDate, setSelectedDate] = useState(null)
+    const [isDateSelected, setIsDateSelected] = useState(false) // New state variable
     const { boardId, groupId, taskId } = useParams()
+    let lastDate = useRef(null)
 
     useEffect(() => {
         onLoadDueDate(boardId, groupId, taskId)
@@ -30,11 +29,20 @@ export function DateEdit({ editName, onCloseEditTask, setTask }) {
             const dueDate = task.dueDate
             if (dueDate) {
                 const formattedDate = dayjs(dueDate, 'MMM D, YYYY [at] h:mm A')
-                setSelectedDate(formattedDate);
+                setSelectedDate(formattedDate)
+                setIsDateSelected(true)
+            } else {
+                setIsDateSelected(false)
             }
         } catch (err) {
             console.log('Can not load due date', err)
         }
+    }
+
+    function onSetSelectedDate(date) {
+        setSelectedDate(date)
+        lastDate.current = date
+        setIsDateSelected(true)
     }
 
     async function onSaveDate() {
@@ -44,6 +52,7 @@ export function DateEdit({ editName, onCloseEditTask, setTask }) {
             await saveTaskDueDate(boardId, groupId, taskId, formatedDate)
             setTask(prevTask => ({ ...prevTask, dueDate: formatedDate }))
             setSelectedDate(selectedDate)
+            setIsDateSelected(true)
             onCloseEditTask('')
         } catch (err) {
             console.log('Could not save date =>', err)
@@ -54,9 +63,21 @@ export function DateEdit({ editName, onCloseEditTask, setTask }) {
         try {
             await removeDueDate(boardId, groupId, taskId)
             setTask(prevTask => ({ ...prevTask, dueDate: null }))
+            setIsDateSelected(false)
             onCloseEditTask('')
         } catch (err) {
             console.log('Cannot remove due date', err)
+        }
+    }
+
+    function onToggleChecked() {
+        if (isDateSelected) {
+            setIsDateSelected(false)
+            setSelectedDate(null)
+        } else {
+
+            setIsDateSelected(true)
+            setSelectedDate(lastDate.current)
         }
     }
 
@@ -71,30 +92,78 @@ export function DateEdit({ editName, onCloseEditTask, setTask }) {
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
 
                         <DateCalendar value={selectedDate}
-                            onChange={(date) => setSelectedDate(date)} sx={{ width: 280 }} />
+                            onChange={(date) => onSetSelectedDate(date)} sx={{
+                                width: 280,
+                                "& .MuiPickersDay-root": {
+                                    '&:hover': {
+                                        bgcolor: 'rgba(9, 30, 66, 0.06)',
+                                        borderRadius: 0,
+                                    },
+                                    fontSize: 14,
+                                    "&.Mui-selected": {
+                                        color: '#0C66E4',
+                                        bgcolor: '#e9f2ff',
+                                        borderRadius: 0,
+                                        '&:active': {
+                                            bgcolor: '#DEEBFF',
+                                            borderRadius: 0,
+                                        },
+                                        '&:focus': {
+                                            bgcolor: '#e9f2ff',
+                                        },
+                                    },
+                                    '&.MuiPickersDay-today': {
+                                        color: '#0C66E4',
+                                        borderRadius: 0,
+                                        border: 'none',
+                                        borderBottom: '2px solid #0C66E4',
+                                    },
+                                },
+                            }}
+                        />
                         <div className='due-date'>
-                            <p>Due date</p>
-                            {/* <DateTimeFieldValue
-                                label="Controlled field"
-                                value={selectedDate}
-                                onChange={(date) => setSelectedDate(date)}
-                            /> */}
-                            {/* <DatePicker value={selectedDate}
-                                onChange={(date) => setSelectedDate(date)} /> */}
-                            <DateField sx={{ width: 108 }} value={selectedDate}
-                                onChange={(date) => setSelectedDate(date)} />
-                            <TimeField sx={{ width: 108 }} value={selectedDate} onChange={(time) => setSelectedDate(time)} />
+                            <p style={isDateSelected ? { color: '#0c66e4' } : {}}>Due date</p>
+                            <section className='due-date-details flex align-center'>
+
+                                <Checkbox checked={isDateSelected} onChange={onToggleChecked}
+                                    sx={{
+                                        p: 0, mr: 1, fontSize: 12, width: 16, height: 16,
+                                        '&.MuiSvgIcon-root': {
+                                            fontSize: 12,
+                                        },
+                                    }} />
+                                <DateField sx={{
+                                    width: 95, mr: 1, fontSize: 14,
+                                    '&:hover': {
+                                        border: 0
+                                    },
+                                    // "& .MuiPickersDay-root": {
+                                    //     borderColor: '#0c66e4',
+                                    // },
+                                    input: { cursor: isDateSelected ? 'pointer' : 'not-allowed' }
+                                }} value={selectedDate}
+                                    onChange={(date) => setSelectedDate(date)} />
+                                <TimeField sx={{
+                                    width: 90, fontSize: 14,
+                                    input: { cursor: isDateSelected ? 'pointer' : 'not-allowed' }
+                                }} value={selectedDate} onChange={(time) => setSelectedDate(time)} />
+                            </section>
                         </div>
                     </LocalizationProvider>
                     <div className='action-btn'>
                         <Button className='btnn' variant="contained" onClick={onSaveDate}
                             sx={{
+                                mb: 1,
+                                mt: 1.5,
                                 color: '#ffff',
                                 borderRadius: 1,
                                 bgcolor: '#0C66E4',
                                 '&:hover': {
                                     bgcolor: '#0850c9'
                                 },
+                                '&:active': {
+                                    bgcolor: '#09326c'
+                                }
                             }}>
                             Save
                         </Button>
@@ -105,14 +174,17 @@ export function DateEdit({ editName, onCloseEditTask, setTask }) {
                                 borderRadius: 1,
                                 bgcolor: ' #091e420f',
                                 '&:hover': {
-                                    bgcolor: ' #091e4224'
+                                    bgcolor: '#091e4224'
                                 },
+                                '&:active': {
+                                    bgcolor: '#091E424F'
+                                }
                             }}>
                             Remove
                         </Button>
                     </div>
                 </div>
-            </section>
+            </section >
         </section >
     )
 }
