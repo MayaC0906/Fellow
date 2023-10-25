75fba7f4602bec891740aad9fc60060452515405
Maya <mayac8771108@gmail.com>
2023-10-25 17:17:49 +0300

    started task cover

--- src/cmps/Dynamic/CoverEdit.jsx
+++ src/cmps/Dynamic/CoverEdit.jsx
@@ -9,6 +9,33 @@ export function CoverEdit({ editName, onCloseEditTask, onSaveTask, task }) {
             </div>
             <section className="edit-modal-content">
                 <div className='content'>
+                    <h2>Size</h2>
+                    <section className="cover-sizes">
+                        <div className="cover-size">
+                            {task.cover.img && <img className="cover-img" src={task.cover.img} alt="cover-img" />}
+                            <div className="cover-footer">
+                                <section className="big-lines">
+                                    <div className="long-line"></div>
+                                    <div className="short-line"></div>
+                                </section>
+                                <div className="footer-bttom">
+                                    <section className="small-lines">
+                                        <div className="small-line"></div>
+                                        <div className="small-line"></div>
+                                    </section>
+                                    <div className="circle"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="cover-size">
+                            {task.cover.img && <img className="cover-img" src={task.cover.img} alt="cover-img" />}
+                                <section className="big-lines">
+                                    <div className="long-line"></div>
+                                    <div className="short-line"></div>
+                                </section>
+                        </div>
+                    </section>
+                    {task.cover.img || task.cover.backgroundColor && <button>Remove cover</button>}
                 </div>
             </section >
         </section >
