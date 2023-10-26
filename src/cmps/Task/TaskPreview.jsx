6f03840eae0f9d303a956fcebf25a53685442b86
Maya <mayac8771108@gmail.com>
2023-10-26 19:30:43 +0300

    F/TaskPreview/ pixel perfect task preview

--- src/cmps/Task/TaskPreview.jsx
+++ src/cmps/Task/TaskPreview.jsx
@@ -2,6 +2,7 @@ import dayjs from "dayjs"
 import { taskSvg } from "../Svgs"
 import { saveNewTask } from "../../store/actions/board.actions"
 import { useParams } from "react-router"
+import { darken } from 'polished';
 
 export function TaskPreview({ task, setIsLabelsShown, isLabelsShown, taskLabels, taskMembers, taskChecklist, groupId }) {
     const { dueDate } = task
@@ -27,33 +28,33 @@ export function TaskPreview({ task, setIsLabelsShown, isLabelsShown, taskLabels,
 
     return (
         <article key={task.id} className="task">
-            <button>{taskSvg.pencil}</button>
+            <button><span className="icon-badge">{taskSvg.pencil}</span></button>
             {task.cover?.backgroundColor && <div className="task-cover" style={{ backgroundColor: task.cover.backgroundColor }}></div>}
             {task.cover?.img && <img className="task-cover" src={task.cover.img} alt="" />}
 
             <section className="task-info">
                 <section className="labels">
                     {taskLabels.map(label => {
-                        return <div key={label.id} className={"label " + (isLabelsShown ? 'open' : 'close')} style={{ backgroundColor: label.color }}
+                        return <div key={label.id} className={"label " + (isLabelsShown ? 'open' : 'close')} style={{ backgroundColor: label.color}}
                             onClick={onLabelOpen} >
-                            {label.title && <span>{label.title}</span>} </div>
+                            {label.title && <span className="label-title" style={{color: darken(0.4, label.color)}}>{label.title}</span>} </div>
                     })}
                 </section>
                 <h3>{task?.title}</h3>
                 <section className="task-badges">
-                    {task.watching && <div className="task-badge">{taskSvg.watch}</div>}
+                    {task.watching && <div className="task-badge"><span className="icon-badge">{taskSvg.watch}</span></div>}
 
                     {dueDate.date && <div className={`task-badge duedate ${dueDate.isComplete ? 'complete' : ''} ${dueDate.isOverdue && !dueDate.isComplete ? 'overdue' : ''}`} onClick={(e) => onCompleteDueDate(e)}>
-                        <span className="clock">{taskSvg.clock}</span>
-                        {dueDate.isComplete ? <span className="checklist">{taskSvg.checklist}</span> : <span className="empty-square">{taskSvg.square}</span>}
-                        <span className="date-data">{dayjs(dueDate.date, 'MMM D [at] h:mm A').format('MMM D, YYYY')}</span>
+                        <span className="clock icon-badge">{taskSvg.clock}</span>
+                        {dueDate.isComplete ? <span className="checklist icon-badge">{taskSvg.checklist}</span> : <span className="empty-square icon-badge">{taskSvg.square}</span>}
+                        <span className="date-data number-badge">{dayjs(dueDate.date, 'MMM D [at] h:mm A').format('MMM D, YYYY')}</span>
                     </div>}
 
-                    {task.description && <div className="task-badge">{taskSvg.description}</div>}
-                    {task.comments?.length > 0 && <div className="task-badge">{taskSvg.comment} <span>{task.comments.length}</span></div>}
-                    {task.attachments?.length > 0 && <div className="task-badge">{taskSvg.attatchment} <span>{task.attachments.length}</span></div>}
-                    {task.checklists?.length > 0 && <div className="task-badge">{taskSvg.checklist}
-                        <span>{taskChecklist.done}/{taskChecklist.all}</span></div>}
+                    {task.description && <div className="task-badge"> <span className="icon-badge">{taskSvg.description}</span></div>}
+                    {task.comments?.length > 0 && <div className="task-badge"><span className="icon-badge">{taskSvg.comment}</span> <span className="number-badge">{task.comments.length}</span></div>}
+                    {task.attachments?.length > 0 && <div className="task-badge"> <span className="icon-badge">{taskSvg.attatchment}</span> <span className="number-badge">{task.attachments.length}</span></div>}
+                    {task.checklists?.length > 0 && <div className={"task-badge " + (taskChecklist.done === taskChecklist.all ? 'complete' : '')}><span className="icon-badge">{taskSvg.checklist}</span>
+                        <span className="number-badge">{taskChecklist.done}/{taskChecklist.all}</span></div>}
                 </section>
 
                 {task.memberIds?.length > 0 &&
