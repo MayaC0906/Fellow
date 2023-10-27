527398d23f0fb07a8bb479d1d4649a7b82b6b85e
SaharMach <saharmach28@gmail.com>
2023-10-27 03:25:07 +0300

    board left sidebar board adding and sorting

--- src/cmps/Board/AddBoard.jsx
+++ src/cmps/Board/AddBoard.jsx
@@ -5,14 +5,15 @@ import { addBoard } from "../../store/actions/board.actions";
 import { useNavigate } from "react-router-dom";
 import { utilService } from "../../services/util.service";
 import { additionTaskSvg } from "../Svgs";
+import { bottom } from "@popperjs/core";
 
-export function AddBoard({ setIsBoardAdded }) {
+export function AddBoard({ pos, setIsBoardAdded }) {
     const imgUrls = ['https://images.unsplash.com/photo-1696384036025-c7d7b7f6584d?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1696580436068-f19c26850e8b?auto=format&fit=crop&q=80&w=2071&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1695754188846-a4a384566dd2?auto=format&fit=crop&q=80&w=2069&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1594743896255-be81f8dfec3d?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://res.cloudinary.com/duvatj8kg/image/upload/v1697202495/aec98becb6d15a5fc95e_dseafo.svg', 'https://res.cloudinary.com/duvatj8kg/image/upload/v1697202522/8ab3b35f3a786bb6cdac_ci3ilc.svg', 'https://res.cloudinary.com/duvatj8kg/image/upload/v1697202531/707f35bc691220846678_bdydef.svg', 'https://res.cloudinary.com/duvatj8kg/image/upload/v1697202537/d106776cb297f000b1f4_kroicr.svg']
-
+    const [getPos, setPos] = useState(pos)
     const [backGroundgImg, setBackGroundImg] = useState('https://images.unsplash.com/photo-1696384036025-c7d7b7f6584d?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
     const [boardTitle, setBoardTtile] = useState('')
     const navigate = useNavigate()
-
+    console.log('getPos', getPos);
     function onSetBoardTtile() {
         let value = event.target.value
         setBoardTtile(value)
@@ -41,7 +42,8 @@ export function AddBoard({ setIsBoardAdded }) {
     }
 
     return (
-        <section style={{ bottom: '290px' }} className="edit-modal">
+        <section style={getPos ? { bottom: '0px', right: '0px' } : { bottom: '290px', right: '130px' }}
+        className="edit-modal">
             <div className="title-container">
                 <p className="add-board-title">Create board</p>
                 <button onClick={() => setIsBoardAdded(false)} className="close-modal">{additionTaskSvg.close}</button>
